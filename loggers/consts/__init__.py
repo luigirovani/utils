556b6f2a76c69cdbe3f90612e3ee382abbd6a427
luigirovani <luigi3520@gmail.com>
2024-11-25 22:27:52 -0300

    FILE HANDLES

--- loggers/consts/__init__.py
+++ loggers/consts/__init__.py
@@ -2,6 +2,7 @@ from.colour_dict import COLOURS, LOG_COLOURS, COLOUR_LOG_PATTERN, COLOUR_LOG_LIG
 from. import normal, colorful
 
 DATEFTM = "%d-%m-%Y %H:%M:%S"
+DATE_ROTATIVE = "%H:%M:%S"
 TIMEZONE = 'America/Sao_Paulo'
 
 
